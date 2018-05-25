da162729b7b9105065963f54290704803af2cdd6
nakamura <kan.nakamura0711@gmail.com>
2018-05-25 13:47:05 +0900

    add about

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
+# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 Rails.application.routes.draw do
   resources :posts
-  get "home/top" => "home#top"
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get "/" => "home#top"
+  get "about" => "home#about"
 end
