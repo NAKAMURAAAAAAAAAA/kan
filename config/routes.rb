27dc90395b671124a25f5fc711f021d0d350bc80
nakamura <kan.nakamura0711@gmail.com>
2018-05-27 18:29:30 +0900

    Add users

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,13 @@ Rails.application.routes.draw do
   get "posts/:id/edit" => "posts#edit"
   post "posts/:id/update" => "posts#update"
   post "posts/:id/destroy" => "posts#destroy"
+
+  post "users/:id/update" => "users#update"
+  get "users/:id/edit" => "users#edit"
+  post "users/create" => "users#create"
+  get "signup" => "users#new"
+  get "users/index" => "users#index"
+  get "users/:id" => "users#show"
   
   get "/" => "home#top"
   get "about" => "home#about"
