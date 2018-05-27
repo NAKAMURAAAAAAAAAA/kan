6f51a2c1d900428c75fccf51472f46cdf7278cd9
nakamura <kan.nakamura0711@gmail.com>
2018-05-27 19:01:37 +0900

    add user_images

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_26_071618) do
+ActiveRecord::Schema.define(version: 2018_05_27_093340) do
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 2018_05_26_071618) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image_name"
   end
 
 end
