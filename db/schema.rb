c3562e2a2ef61e4701f1f66841224feaeacab169
nakamura <kan.nakamura0711@gmail.com>
2018-05-27 23:33:42 +0900

    connect user and post

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_27_115551) do
+ActiveRecord::Schema.define(version: 2018_05_27_121643) do
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2018_05_27_115551) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.text "content"
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
