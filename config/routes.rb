cf5643a27baf35ae06e55246d747f041b9496cbb
nakamura <kan.nakamura0711@gmail.com>
2018-05-27 23:58:33 +0900

    add like function

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,17 @@
 Rails.application.routes.draw do
-  get "login" => "users#login_form"
+  post "likes/:post_id/create" => "likes#create"
+  post "likes/:post_id/destroy" => "likes#destroy"
+
+  post "users/:id/update" => "users#update"
+  get "users/:id/edit" => "users#edit"
+  post "users/create" => "users#create"
+  get "signup" => "users#new"
+  get "users/index" => "users#index"
+  get "users/:id" => "users#show"
   post "login" => "users#login"
   post "logout" => "users#logout"
+  get "login" => "users#login_form"
+  get "users/:id/likes" => "users#likes"
 
   get "posts/index" => "posts#index"
   get "posts/new" => "posts#new"
@@ -11,13 +21,6 @@ Rails.application.routes.draw do
   post "posts/:id/update" => "posts#update"
   post "posts/:id/destroy" => "posts#destroy"
 
-  post "users/:id/update" => "users#update"
-  get "users/:id/edit" => "users#edit"
-  post "users/create" => "users#create"
-  get "signup" => "users#new"
-  get "users/index" => "users#index"
-  get "users/:id" => "users#show"
-  
   get "/" => "home#top"
   get "about" => "home#about"
 end
