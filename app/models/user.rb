349696604e4d7d66a57d0a3257cb3b4beca89606
nakamura <kan.nakamura0711@gmail.com>
2018-05-27 21:04:05 +0900

    add login&logout

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
-end
+    validates :name, {presence: true}
+    validates :email, {presence: true, uniqueness: true}
+    validates :password, {presence: true}
+  end
+  
